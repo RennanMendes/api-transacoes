4300dfbb8e31b1c00b23f81eaccd997b2eda1dc4
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2025-02-01 17:14:02 -0300

    feat: adiciona endpoint de limpar transações

--- src/main/java/br/com/itau/api_transacoes/api/controller/TransacaoController.java
+++ src/main/java/br/com/itau/api_transacoes/api/controller/TransacaoController.java
@@ -3,33 +3,33 @@ package br.com.itau.api_transacoes.api.controller;
 import br.com.itau.api_transacoes.api.converter.TransacaoConverter;
 import br.com.itau.api_transacoes.api.dto.TransacaoDto;
 import br.com.itau.api_transacoes.core.cases.AdicionarTransacaoUseCase;
+import br.com.itau.api_transacoes.core.cases.LimparTransacoesUseCase;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
+@AllArgsConstructor
 public class TransacaoController {
 
     private final AdicionarTransacaoUseCase adicionarTransacao;
+    private final LimparTransacoesUseCase limparTransacoes;
     private final TransacaoConverter converter;
-
-    @Autowired
-    public TransacaoController(AdicionarTransacaoUseCase adicionarTransacao, TransacaoConverter converter) {
-        this.adicionarTransacao = adicionarTransacao;
-        this.converter = converter;
-    }
 
     @PostMapping
     public ResponseEntity adicionarTransacao(@Valid @RequestBody TransacaoDto dto) {
         adicionarTransacao.adicionar(converter.dtoToEntity(dto));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @DeleteMapping
+    public ResponseEntity deletarTransacoes() {
+        limparTransacoes.limpar();
+        return ResponseEntity.ok().build();
+    }
+
 
 }
